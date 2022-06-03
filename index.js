48449d10304fcc118c3f36c37b737a78ea51625f
Alena Provotorova <lunichkina.alena@gmail.com>
2022-06-03 14:10:26 +0300

    files

---
+++ index.js
@@ -0,0 +1,17 @@
+import { createServer } from '@graphql-yoga/node'
+import {join} from 'path'
+import {readFileSync} from 'fs'
+import resolvers from './graphql/resolvers'
+
+const typeDefs = readFileSync(join(process.cwd(), 'schema.graphql'), {
+    encoding: 'utf-8'
+})
+
+const server = new createServer({
+    schema: {
+        typeDefs,
+        resolvers
+    }
+})
+
+server.start(() => console.log('=====success=====', ))
